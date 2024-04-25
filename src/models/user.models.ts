c9ec318da6e3bce0372c209ee39c7be0490877c3
Ricardo Bermudez <ribermudez@petalmail.com>
2024-04-24 22:02:15 -0500

    feat: import prisma

--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,5 +1,6 @@
 import type { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import prisma from "~/server/db";
 
 export async function createUser(email: User["email"], password: string) {
   const hashedPassword = await bcrypt.hash(password, 10);
