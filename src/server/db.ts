f1a53ac548c1938fef7009024ec95af1b35793e8
Ricardo Bermudez <ribermudez@petalmail.com>
2024-04-24 22:18:17 -0500

    feat: create a singleton prisma

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,6 +4,8 @@ declare global {
     var prisma: PrismaClient;
 }
 
+let prisma: PrismaClient | null = null;
+
 if (typeof window === "undefined") {
     if (process.env.NODE_ENV === "production") {
         prisma = new PrismaClient();
